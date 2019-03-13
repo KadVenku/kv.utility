494fa27006d15d141e1dd6b44d2bfc0c8d24c6e0
KadVenku <venkukad@gmail.com>
2019-03-13 19:44:23 +0100

    Added EnumUtility

---
+++ kv.util/EnumUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace kv.util
+{
+    public sealed class EnumUtility<T>
+    {
+        public static IEnumerable<T> GetValues()
+        {
+            IEnumerable<T> vals = Enum.GetValues(typeof(T)).Cast<T>();
+            return vals;
+        }
+    }
+}
